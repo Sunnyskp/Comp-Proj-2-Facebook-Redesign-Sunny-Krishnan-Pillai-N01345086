915a0aed31c1df5a7ce1ad35b25f85edac832140
Sunnyskp <skp2june@gmail.com>
2019-04-14 03:10:14 -0400

    Added ViewPost and CloseVeiwPost functions

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const eleUserInput = document.getElementById("UserInput")
 const eleProfPic = document.getElementById("profPic")
 const storePosts = [];
 const newPost = () => {
-  storePosts.unshift(`<div class="column is-one-quarter is-mobile is-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
+  storePosts.unshift(`
+<div onclick="viewPost()" class="column is-mobile is-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
   <article class="media">
     <figure class="media-left">
       <p class="image is-64x64 is-rounded">
@@ -14,7 +15,8 @@ const newPost = () => {
       <div class="content">
         <p>
           <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-          <br>${eleUserInput.value}</p>
+          ${eleUserInput.value}
+        </p>
       </div>
       <nav class="level is-mobile">
         <div class="level-left">
@@ -46,6 +48,68 @@ const newPost = () => {
   localStorage.getItem("FacebookPosts",post2String);
   const string2Post = JSON.parse(post2String);
   eleFbPosts.innerHTML = string2Post;
+}
+
+let viewPost = () => {
+  eleFbPosts.innerHTML = `
+  <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Modal title</p>
+      <button onclick="closeViewPost()" class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+    <div onclick="viewPost()" class="column is-one-quarter is-mobile is-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
+    <article class="media">
+      <figure class="media-left">
+        <p class="image is-64x64 is-rounded">
+          ${eleProfPic.innerHTML}<br>
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+            ${eleUserInput.value}
+          </p>
+        </div>
+        <nav class="level is-mobile">
+          <div class="level-left">
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-comment"></i></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-share-square"></i></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-heart"></i></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-thumbs-up"></i></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-edit"></i></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fas fa-trash-alt"></i></span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </article>
+  </div>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Save changes</button>
+      <button onclick="closeViewPost()" class="button">Cancel</button>
+    </footer>
+  </div>
+</div>`;
+}
+
+const closeViewPost = () => {
+  window.location = "index.html";
 }
 
 let attempt = 3;
