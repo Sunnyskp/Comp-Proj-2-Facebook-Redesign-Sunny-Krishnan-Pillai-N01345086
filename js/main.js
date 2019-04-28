2267ab4437318ab91aac997b72caaf617d08873c
Sunnyskp <skp2june@gmail.com>
2019-04-27 21:51:41 -0400

    Multiuser homepage display added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const eleFbPosts = document.getElementById("fbPosts");
 const eleUserInput = document.getElementById("UserInput");
 const eleProfPic = document.getElementById("profPic");
+let displayUserName = JSON.parse(localStorage.getItem("Username"));
 let samplePost2String = [];
 let post2String = [];
 
@@ -15,13 +16,13 @@ defaultPosts = () => {
 <article class="media">
   <figure class="media-left">
     <p class="image is-64x64 is-rounded">
-        ${eleProfPic.innerHTML}<br>
+    <img  class="image is-64x64 is-rounded"  src="../img/prof-pic-1.jpg"><br>
     </p>
   </figure>
   <div  class="media-content">
     <div onclick="viewPost()" class="content">
       <p>
-        <strong>Users Name</strong> <small>@Username</small> <br>
+        <strong>Stephen Devasy</strong> <small>@stephen</small> <br>
         <div class="fb-post-figure-style">
               <img src="http://images2.fanpop.com/images/photos/8400000/Cinderella-cinderella-8493295-150-260.gif"  class="fb-post-image-style" >
         </div>
@@ -37,13 +38,13 @@ defaultPosts = () => {
 <article class="media">
   <figure class="media-left">
     <p class="image is-64x64 is-rounded">
-        ${eleProfPic.innerHTML}<br>
+    <img  class="image is-64x64 is-rounded"  src="../img/prof-pic-2.jpg"><br>
     </p>
   </figure>
   <div  class="media-content">
     <div onclick="viewPost()" class="content">
       <p>
-        <strong>Users Name</strong> <small>@Username</small> <br>
+        <strong>Sony Pillai</strong> <small>@Sony</small> <br>
         <div class="fb-post-figure-style">
               <img src="https://png.pngtree.com/thumb_back/fh260/back_pic/04/10/85/2658188a5fd29a6.jpg"  class="fb-post-image-style" >
         </div>
@@ -59,13 +60,13 @@ defaultPosts = () => {
 <article class="media">
   <figure class="media-left">
     <p class="image is-64x64 is-rounded">
-        ${eleProfPic.innerHTML}<br>
+    <img  class="image is-64x64 is-rounded"  src="../img/prof-pic-3.jpg"><br>
     </p>
   </figure>
   <div  class="media-content">
     <div onclick="viewPost()" class="content">
       <p>
-        <strong>Users Name</strong> <small>@Username</small> <br>
+        <strong>Steffy Sunny</strong> <small>@Steffy</small> <br>
         <div class="fb-post-figure-style">
         <p>Infuse your life with action. Don't wait for it to happen. Make it happen. Make your own future. Make your own hope. Make your own love. And whatever your beliefs, honor your creator, not by passively waiting for grace to come down from upon high, but by doing what you can to make grace happen... yourself, right now, right down here on Earth...</p>
         </div>
@@ -81,13 +82,13 @@ defaultPosts = () => {
 <article class="media">
   <figure class="media-left">
     <p class="image is-64x64 is-rounded">
-        ${eleProfPic.innerHTML}<br>
+    <img  class="image is-64x64 is-rounded"  src="../img/prof-pic-4.jpg"><br>
     </p>
   </figure>
   <div  class="media-content">
     <div onclick="viewPost()" class="content">
       <p>
-        <strong>Users Name</strong> <small>@Username</small> <br>
+        <strong>K R Pillai</strong> <small>@Pillai</small> <br>
         <div class="fb-post-figure-style">
         <img src="https://i.pinimg.com/236x/ec/34/c6/ec34c63209d7382e43433818d9012cb0--namaste-portal.jpg"  class="fb-post-image-style" >
         </div>
@@ -103,13 +104,13 @@ defaultPosts = () => {
 <article class="media">
   <figure class="media-left">
     <p class="image is-64x64 is-rounded">
-        ${eleProfPic.innerHTML}<br>
+    <img  class="image is-64x64 is-rounded"  src="../img/prof-pic-5.jpg"><br>
     </p>
   </figure>
   <div  class="media-content">
     <div onclick="viewPost()" class="content">
       <p>
-        <strong>Users Name</strong> <small>@Username</small> <br>
+        <strong>Vimala Ravi</strong> <small>@Vimala</small> <br>
         <div class="fb-post-figure-style">
               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyAa633KAfsEtV_sZ-k6OTBCqBcT6GCs7iBLwb-jKdeoEVCESU2A"  class="fb-post-image-style" >
         </div>
@@ -125,13 +126,13 @@ defaultPosts = () => {
 <article class="media">
   <figure class="media-left">
     <p class="image is-64x64 is-rounded">
-        ${eleProfPic.innerHTML}<br>
+    <img  class="image is-64x64 is-rounded"  src="../img/prof-pic-6.jpg"><br>
     </p>
   </figure>
   <div  class="media-content">
     <div onclick="viewPost()" class="content">
       <p>
-        <strong>Users Name</strong> <small>@Username</small> <br>
+        <strong>Roy Mathew</strong> <small>@Roy</small> <br>
         <div class="fb-post-figure-style">
               <p>Stay true to yourself, yet always be open to learn. Work hard, and never give up on your dreams, even when nobody else believes they can come true but you. These are not cliches but real tools you need no matter what you do in life to stay focused on your path...</p>
         </div>
@@ -147,13 +148,13 @@ defaultPosts = () => {
 <article class="media">
   <figure class="media-left">
     <p class="image is-64x64 is-rounded">
-        ${eleProfPic.innerHTML}<br>
+    <img class="image is-64x64 is-rounded" src="../img/prof-pic-7.jpg"><br>
     </p>
   </figure>
   <div  class="media-content">
     <div onclick="viewPost()" class="content">
       <p>
-        <strong>Users Name</strong> <small>@Username</small> <br>
+        <strong>Shyly Roy</strong> <small>@Shyly</small> <br>
         <div class="fb-post-figure-style">
         <p>No matter what has happened to you in the past or what is going on in your life right now, it has no power to keep you from having an amazingly good future if you will walk by faith in God. God loves you! He wants you to live with victory over sin so you can possess His promises for your life today...!</p>
         </div>
@@ -182,7 +183,7 @@ const newPost = (storePosts = `Share Your Mind..!!!`) => {
     <div  class="media-content">
       <div onclick="viewPost()" class="content">
         <p>
-          <strong>Users Name</strong> <small>@Username</small> <br>
+          <strong>${displayUserName}</strong> <small>@${displayUserName}</small> <br>
           <div>${eleUserInput.value}</div>
         </p>
       </div>
@@ -248,11 +249,11 @@ const viewPost = () => {
 
 const searchPost = () => {
   let eleSearch = document.documentElement.innerHTML;
-  let n = eleSearch.search(`${document.getElementById('searchInput').value}`);
-  if (n == -1) {
+  let searchItem = eleSearch.search(`${document.getElementById('searchInput').value}`);
+  if (searchItem == -1) {
     alert(`Search Not Found`);
   } else {
-    alert(`*****  Found in Character number ${n}  *****`);
+    alert(`*****  Found in Character number ${searchItem}  *****`);
   }
   document.getElementById('searchInput').value = ``;
 }
